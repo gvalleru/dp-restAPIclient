77343ecbac45c2f1c7dd97aa0464647496ada95d
gvalleru <gvalleru@users.noreply.github.com>
2019-02-01 10:12:42 -0800

    Intial commit

---
+++ delete_certs.py
@@ -0,0 +1,36 @@
+import dpRESTclient
+import yaml
+
+
+conf_yaml = 'config.yaml'
+with open(conf_yaml, 'r') as c:
+    config = yaml.load(c)
+
+with open('delete_certs.txt') as f:
+    delete_certs = f.readlines()
+    for line in delete_certs:
+        server = str(line.split()[0].replace("'", ""))
+        cert = str(line.split()[1].replace("'", ""))
+        domain = str(line.split()[2]).replace("'", "")
+
+        if server in config:
+            host = config[server]['hostname']
+            port = config[server]['port']
+            username = config[server]['username']
+            password = config[server]['password']
+            proxies = config[server]['proxies']
+            print "Processing request to remove cert {} from domain {} on {} i.e {}". format(cert,
+                                                                                             domain,
+                                                                                             server,
+                                                                                             host)
+            dp = dpRESTclient.DpRestClient(host, port, username, password, proxies)
+            dp.remove_cert_in_crypto_val_cred(domain, cert)
+            dp.save_config(domain)
+            dp.remove_cert_from_domain(domain, cert)
+            dp.save_config(domain)
+
+        else:
+            print "{0} is not found in {1}. Cant remove {2} from domain {3} on {0}".format(server,
+                                                                                           conf_yaml,
+                                                                                           cert,
+                                                                                           domain)
---
+++ dpRESTclient.py
@@ -0,0 +1,107 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class DpRestClient:
+    def __init__(self, host, port, username, password, proxies, verify=False):
+        self.host = host
+        self.port = str(port)
+        self.username = username
+        self.password = password
+        self.proxies = proxies
+        self.verify = verify
+
+    def _dp_api_resp_dict(self, url):
+        res = requests.get(
+            url,
+            auth=HTTPBasicAuth(username=self.username, password=self.password),
+            proxies=self.proxies,
+            verify=self.verify)
+
+        return json.loads(res.content)
+
+    def _get_cert_from_val_cred(self, val_cred, cert_obj):
+        crypto_val_cred = None
+        certs = None
+
+        if "Certificate" in val_cred:
+            if isinstance(val_cred["Certificate"], list):
+                for cert in val_cred["Certificate"]:
+                    if cert["value"] == cert_obj:
+                        val_cred["Certificate"].remove(cert)
+                        certs = [i["value"] for i in val_cred["Certificate"]]
+                        crypto_val_cred = val_cred["name"]
+                        return crypto_val_cred, certs
+            else:
+                if val_cred["Certificate"]["value"] == cert_obj:
+                    del val_cred["Certificate"]
+                    certs = None
+                    crypto_val_cred = val_cred["name"]
+                    return crypto_val_cred, certs
+
+            return crypto_val_cred, certs
+        else:
+            return crypto_val_cred, certs
+
+    def _update_certs_in_val_cred(self, domain, val_cred, certs):
+        url = "https://" + self.host + ":" + self.port + "/mgmt/config/" + domain + "/CryptoValCred/" + val_cred["name"]
+        del val_cred['_links']
+        val_cred['Certificate'] = certs
+        data = json.dumps({"CryptoValCred": val_cred})
+        r = requests.put(
+            url,
+            data=data,
+            auth=HTTPBasicAuth(username=self.username, password=self.password),
+            proxies=self.proxies,
+            verify=self.verify)
+        return r.status_code, json.loads(r.content)
+        # print self._dp_api_resp_dict(url)
+
+    def save_config(self, domain):
+        url = "https://" + self.host + ":" + self.port + "/mgmt/actionqueue/" + domain
+        data = '{"SaveConfig":""}'
+        res = requests.post(url,
+                            data=data,
+                            auth=HTTPBasicAuth(username=self.username, password=self.password),
+                            proxies=self.proxies,
+                            verify=self.verify)
+        if res.status_code == 200:
+            print "configuration saved"
+
+    def remove_cert_from_domain(self, domain, cert_obj):
+        url = "https://" + self.host + ":" + self.port + "/mgmt/config/"+domain+"/CryptoCertificate/"+cert_obj
+        res = requests.delete(url,
+                              auth=HTTPBasicAuth(username=self.username, password=self.password),
+                              proxies=self.proxies,
+                              verify=self.verify)
+        res_dict = json.loads(res.content)
+        if res.status_code == 200:
+            print "Certificate object {} deleted from {}".format(cert_obj, domain)
+        else:
+            print "Issue in removing Cert object {} from {}".format(cert_obj, domain)
+            print "Error: {}".format(res_dict["error"])
+
+    def remove_cert_in_crypto_val_cred(self, domain, cert_obj):
+        url = "https://"+self.host+":"+self.port+"/mgmt/config/"+domain+"/CryptoValCred"
+        res_dict = self._dp_api_resp_dict(url)
+        if isinstance(res_dict["CryptoValCred"], list):
+            for CryptoValCred in res_dict["CryptoValCred"]:
+                final_crypto_val_cred, final_certs = self._get_cert_from_val_cred(CryptoValCred, cert_obj)
+                if final_crypto_val_cred is not None:
+                    resp_code, resp_content = self._update_certs_in_val_cred(domain, CryptoValCred, final_certs)
+                    if resp_code == 200:
+                        print "Removed {} from {} on domain {}".format(cert_obj, final_crypto_val_cred,
+                                                                       domain)
+
+        else:
+            final_crypto_val_cred, final_certs = self._get_cert_from_val_cred(res_dict["CryptoValCred"],
+                                                                              cert_obj)
+            if final_certs is not None:
+                resp_code, resp_content = self._update_certs_in_val_cred(domain, res_dict["CryptoValCred"], final_certs)
+                print resp_code
+                if resp_code == 200:
+                    print "Removed {} from {} in domain {}".format(cert_obj, final_crypto_val_cred, domain)
