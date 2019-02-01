6f233de9d801a2c23a456435dbb9dc9f6335d034
gvalleru <36757098+gvalleru@users.noreply.github.com>
2019-02-01 10:17:54 -0800

    Delete temp.py

--- temp.py
+++
@@ -1,7 +0,0 @@
-import requests
-from requests.auth import HTTPBasicAuth
-
-res = requests.post('https://dmzdp3.mes1.prod.ce:5554/mgmt/actionqueue/default', data='{"SaveConfig":""}', auth=HTTPBasicAuth(username='admin', password='Cashedge1$'), proxies = {'https': 'http://localhost:3128'}, verify=False)
-print res.content
-print res.status_code
-print res.text
